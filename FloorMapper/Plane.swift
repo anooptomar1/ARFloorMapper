cffa44c2f2979bafba7f73dde657d22f2719089a
Florian Schoeler <fschoeler@itemis.de>
2017-09-08 10:06:27 +0200

    - replace plane geometry with polygon representation - deactivate plane updating

--- FloorMapper/Plane.swift
+++ FloorMapper/Plane.swift
@@ -14,17 +14,35 @@ class Plane: SCNNode {
         self.anchor = anchor
         super.init()
         
-        let geometry = SCNPlane(width: CGFloat(anchor.extent.x), height: CGFloat(anchor.extent.z))
+        let width = anchor.extent.x
+        let height = anchor.extent.z
+        let halfWidth = -width / 2.0
+        let halfHeight = -height / 2.0
+        
+        let positionTopLeft = SCNVector3Make(-halfWidth, 0.0, -halfHeight)
+        let positionTopRight = SCNVector3Make(halfWidth, 0.0, -halfHeight)
+        let positionLowerLeft = SCNVector3Make(-halfWidth, 0.0, halfHeight)
+        let positionLowerRight = SCNVector3Make(halfWidth, 0.0, halfHeight)
+        let positions = [positionTopLeft, positionTopRight, positionLowerLeft, positionLowerRight]
+        
+        let indices : [UInt8] = [0,2,1, 1,2,3]
+        
+        let geometrySource = SCNGeometrySource(vertices: positions)
+        
+        let geometryData = Data(bytes: indices)
+        let geometryElement = SCNGeometryElement(data: geometryData, primitiveType: .triangles, primitiveCount: 2, bytesPerIndex: 1)
+        
+        let geometryPolygon = SCNGeometry(sources: [geometrySource], elements: [geometryElement])
+        
+        let geometryPolygonNode = SCNNode(geometry: geometryPolygon)
+        geometryPolygonNode.position = SCNVector3Make(anchor.center.x, 0, anchor.center.z)
+        
         let red = CGFloat(Float(arc4random()) / Float(UINT32_MAX))
         let green = CGFloat(Float(arc4random()) / Float(UINT32_MAX))
         let blue = CGFloat(Float(arc4random()) / Float(UINT32_MAX))
-        geometry.materials.first?.diffuse.contents = UIColor(red: red, green: green, blue: blue, alpha: 0.6)
+        geometryPolygon.materials.first?.diffuse.contents = UIColor(red: red, green: green, blue: blue, alpha: 0.6)
         
-        let geometryNode = SCNNode(geometry: geometry)
-        geometryNode.position = SCNVector3Make(anchor.center.x, 0, anchor.center.z)
-        geometryNode.transform = SCNMatrix4MakeRotation(Float(-Double.pi / 2.0), 1.0, 0.0, 0.0)
-        
-        addChildNode(geometryNode)
+        addChildNode(geometryPolygonNode)
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -36,12 +54,12 @@ class Plane: SCNNode {
     func update(_ anchor: ARPlaneAnchor) {
         self.anchor = anchor
         
-        if let plane = childNodes.first {
-            let geometry = plane.geometry as! SCNPlane
-            geometry.width = CGFloat(anchor.extent.x)
-            geometry.height = CGFloat(anchor.extent.z)
-            plane.position = SCNVector3Make(anchor.center.x, 0, anchor.center.z)
-        }
+//        if let plane = childNodes.first {
+//            let geometry = plane.geometry as! SCNPlane
+//            geometry.width = CGFloat(anchor.extent.x)
+//            geometry.height = CGFloat(anchor.extent.z)
+//            plane.position = SCNVector3Make(anchor.center.x, 0, anchor.center.z)
+//        }
     }
     
 }
